654b6e24fdaa80f0a88c80334400897b273f1e75
Anfernee Magbutay <fern.magbutay@synacy.com>
2022-07-21 18:18:51 +0800

    Add unit tests for PersonSpec.groovy and fix some unit tests for PersonSpec.groovy

--- src/test/groovy/com/synacy/gradprogram/spock/AddressSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/AddressSpec.groovy
@@ -6,84 +6,84 @@ class AddressSpec extends Specification {
     def "getCountry should return the country of Address"() {
         given:
         def ph_address = new Address("Philippines", "Cebu City", "Escario")
+
         when:
-        // TODO implement stimulus
         String countryLocation = ph_address.country
+
         then:
-        // TODO implement assertions
         countryLocation == "Philippines"
     }
-//
+
     def "setCountry should demonstrate"() {
         given:
         def phAddress = new Address("Cuba", "Talisay", "Escario")
 
         when:
         phAddress.setCountry("Philippines")
-        // TODO implement stimulus
+
         then:
         phAddress.getCountry() == "Philippines"
-        // TODO implement assertions
+
     }
-//
+
     def "getCity should return the city of Address"() {
         given:
         def phAddress = new Address("Philippines", "Cebu City", "Escario")
         when:
         String cityLocation = phAddress.city
-        // TODO implement stimulus
+
         then:
         "Cebu City" == cityLocation
-        // TODO implement assertions
+
     }
-//
-    def "setCity"() {
+
+    def "setCity should update the given City"() {
         given:
         def phAddress = new Address("PH", "Cebu", "Escario")
         when:
         phAddress.setCountry("Talisay")
-        // TODO implement stimulus
+
         then:
         phAddress.getCountry() == "Cebu"
-        // TODO implement assertions
+
     }
-//
+
     def "getStreet should return the street of Address"() {
         given:
         def phAddress = new Address("PH", "Cebu", "Escario")
+
         when:
         String streetLocation = phAddress.getStreet()
-        // TODO implement stimulus
+
         then:
         "Escario" == streetLocation
-        // TODO implement assertions
+
     }
-//
-    def "test setStreet"() {
+
+    def "setStreet should update the given Street"() {
         given:
         def phAddress = new Address("Philippines", "Cebu", "Escario")
+
         when:
-        // TODO implement stimulus
-        phAddress.setCountry("Escario")
+        phAddress.setStreet("Escario")
+
         then:
-        phAddress.getCountry() == "Escario"
-        // TODO implement assertions
+        phAddress.getStreet() == "Escario"
+
     }
-//
-    def "test toString"() {
+
+    def "toString should provide a string of the parameters initialized in Address"() {
         given:
         def phAddress = new Address("Philippines", "Cebu", "Escario")
+
         when:
         def message = phAddress.toString()
-        // TODO implement stimulus
+
         then:
         message == "Country: " + phAddress.country +
                 ", City: " + phAddress.city +
                 ", Street: " + phAddress.street
-        // TODO implement assertions
+
 
     }
-//        public static void checkAddressParameters() {
-//
-//        }
 }
--- src/test/groovy/com/synacy/gradprogram/spock/PersonSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/PersonSpec.groovy
@@ -5,86 +5,114 @@ import spock.lang.Specification
 
 class PersonSpec extends Specification {
 
-    def "test getName"() {
+    def "getName should return the name of the initialized person in class Person"() {
         given:
-        Person person = Mock()
-        Sex sex = Mock()
+        //def phAddress = new Address("PH", "Cebu", "Escario")
+        Address address = Mock()
 
-        person.setName() >>
         when:
+        def person = new Person("Mark", 20, address, Sex.MALE)
+        then:
+        person.getName() == "Mark"
+    }
 
+    def "setName should update the name of the initialized person in class Person"() {
+        given:
+        Address address = Mock()
+
+        when:
+        def person = new Person("Jack", 20, address, Sex.MALE)
+        person.setName("MrKrabs")
 
         then:
+        person.getName() == "MrKrabs"
+    }
+    def "getAge should return the age of the initialized person in class Person() {
+        given:
+        Address address = Mock()
 
-//    def "test setName"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test getAge"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setAge"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test getAddress"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setAddress"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test getSex"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test setSex"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
-//
-//    def "test toString"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
+        when:
+        def person = new Person("Mark", 22, address, Sex.MALE)
+
+        then:
+        person.getAge() == 22
+    }
+
+    def "setAge should update the age of the initialized person in class Person"() {
+        given:
+        Address address = Mock()
+
+        when:
+        def person = new Person("Jack", 20, address, Sex.MALE)
+        person.setAge(23)
+
+        then:
+        person.getAge() == 23
+    }
+
+    def "getAddress should return the given address of an initialized person of Class Person"() {
+        given:
+        def phAddress = new Address("PH", "Cebu", "Escario")
+        def wrongAddress = new Address("wrong", "place", "wrong time")
+
+        when:
+        def person = new Person("Mark", 20, phAddress, Sex.MALE)
+
+        then:
+//      person.getAddress() == wrongAddress //demonstrates if it were to be a wrong address
+        person.getAddress() == phAddress
+        }
+
+    def "setAddress should update the Address parameter of an intialized person in Class Person"() {
+        given:
+        def phAddress = new Address("PH", "Cebu", "Escario")
+        def wrongAddress = new Address("wrong", "place", "wrong time")
+
+        when:
+        def person = new Person("Mark", 20, phAddress, Sex.MALE)
+        person.setAddress(wrongAddress)
+
+        then:
+        person.getAddress() == wrongAddress
+    }
+
+    def "getSex should return the sex of an initialized person of Class Person"() {
+        given:
+        Address address = Mock()
+
+        when:
+        def person = new Person("Martina", 20, address, Sex.FEMALE)
+
+        then:
+        person.getSex() == Sex.FEMALE
+
+    }
+
+    def "setSex should update the sex parameter of an initialized person of Class Person"() {
+        given:
+        Address address = Mock()
+        def person = new Person("Jack", 20, address, Sex.MALE)
+
+        when:
+        person.setSex(Sex.FEMALE)
+
+        then:
+        person.getSex() == Sex.FEMALE
+    }
+
+    def "toString should demonstrate the given parameters initialized from Class Person"() {
+        given:
+        Address address = new Address("PH", "Cebu", "Escario")
+        Person person = new Person("Jack", 20, address, Sex.MALE)
+
+        when:
+        def message = person.toString()
+
+        then:
+        message == "Name: " + person.name +
+                ", Age: " + person.age +
+                ", Address: (" + address.toString() + ")" +
+                ", Sex: " + person.sex
+    }
+
 }
