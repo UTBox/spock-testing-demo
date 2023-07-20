9e72b9537e8c66f2a4a6aeaa3c9625592a4a5417
junsaku0 <junsaku.yamada@synacy.com>
2023-07-20 16:33:09 +0800

    Create files for testing DeliveryService and ShoppingService

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/DeliveryServiceSpec.groovy
@@ -0,0 +1,11 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class DeliveryServiceSpec extends Specification {
+    void setup() {
+    }
+
+    def "CreateDelivery"() {
+    }
+}
---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/ShoppingServiceSpec.groovy
@@ -0,0 +1,14 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class ShoppingServiceSpec extends Specification {
+    void setup() {
+    }
+
+    def "BuyNonSpoilingItemsInCart"() {
+    }
+
+    def "GetOrderSummary"() {
+    }
+}
