b37b33623092d4a79619676738f26fcfc79896b5
junsaku0 <junsaku.yamada@synacy.com>
2023-07-20 17:29:00 +0800

    Create test for DeliveryService

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/DeliveryServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/DeliveryServiceSpec.groovy
@@ -1,11 +1,46 @@
 package com.synacy.gradprogram.spock.exercise
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class DeliveryServiceSpec extends Specification {
+
+    DeliveryRequestRepository deliveryRequestRepository
+    Order order
+    DeliveryService deliveryService
+    DateUtils dateUtils
+
     void setup() {
+        deliveryRequestRepository = Mock(DeliveryRequestRepository)
+        order = Mock(Order)
+        dateUtils = Mock(DateUtils)
+        deliveryService = new DeliveryService(dateUtils, deliveryRequestRepository)
     }
 
-    def "CreateDelivery"() {
+    def "CreateDelivery should save OrderID, DeliveryDate and Courier to deliveryRequestRepository"() {
+        given:
+        UUID expectedId = UUID.randomUUID()
+        order.getId() >> expectedId
+
+        def expectedDeliveryDate = new Date()
+        dateUtils.getCurrentDate() >> expectedDeliveryDate
+
+        order.getTotalCost() >> cost
+
+        when:
+        deliveryService.createDelivery(order)
+
+        then:
+        1 * deliveryRequestRepository.saveDeliveryRequest(_) >> { DeliveryRequest deliveryRequest ->
+            assert expectedId == deliveryRequest.getOrderId()
+            assert expectedDeliveryDate == deliveryRequest.getDeliveryDate()
+            assert expectedCourier == deliveryRequest.getCourier()
+        }
+
+        where:
+        cost     | expectedCourier
+        15       | Courier.JRS
+        25       | Courier.GRAB
+        35       | Courier.LBC
     }
 }
