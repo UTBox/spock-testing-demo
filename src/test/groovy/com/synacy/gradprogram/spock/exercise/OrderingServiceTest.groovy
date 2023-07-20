b4e2b9add7e599e53d753580e2c14edb1a2189c1
clark <clark.tabar@synacy.com>
2023-07-20 21:48:40 +0800

    added unit test for calculateTotalCostofCart, isCartEligibleForDiscount and applyDiscountToCartItems

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceTest.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceTest.groovy
@@ -40,11 +40,90 @@ class OrderingServiceTest extends Specification {
 
             Cart itemsInTheCart = new Cart(UUID.randomUUID(), items)
 
-
         when:
             boolean hasFoodItems = service.cartContainsFoodItem(itemsInTheCart)
 
         then:
             !hasFoodItems
     }
+
+    def "calculateTotalCostOfCart should get the sum of totalPrice."() {
+        given:
+            Item nuggets = new Item("Nuggets", 185, ItemType.FOOD)
+            Item ham = new Item("Ham", 198, ItemType.FOOD)
+            Item biscuit = new Item("Biscuit", 49, ItemType.GADGET)
+            Item appleJuice = new Item("Apple Juice", 174, ItemType.FOOD)
+
+            List<Item> items = [nuggets, ham, biscuit, appleJuice]
+
+            Cart itemsInTheCart = new Cart(UUID.randomUUID(), items)
+
+        when:
+            double actualTotalPrice = service.calculateTotalCostOfCart(itemsInTheCart)
+
+        then:
+            actualTotalPrice
+
+    }
+
+    def "isCartEligibleForDiscount should validate if the total price from the cart is eligible for discount."() {
+        given:
+        Item nuggets = new Item("Nuggets", 185.0, ItemType.FOOD)
+        Item ham = new Item("Ham", 198.0, ItemType.FOOD)
+        Item biscuit = new Item("Biscuit", 49.0, ItemType.GADGET)
+        Item appleJuice = new Item("Apple Juice", 174.0, ItemType.FOOD)
+        Item milk = new Item("Milk", 98.0, ItemType.FOOD)
+        Item chocolate = new Item("Chocolate", 357.0, ItemType.FOOD)
+
+        List<Item> items = [nuggets, ham, biscuit, appleJuice, milk, chocolate]
+
+        Cart itemsInTheCart = new Cart(UUID.randomUUID(), items)
+
+        when:
+        boolean eligibleForDiscount = service.isCartEligibleForDiscount(itemsInTheCart)
+
+        then:
+        eligibleForDiscount
+
+    }
+
+    def "isCartEligibleForDiscount should validate if the total price from the cart is not eligible for discount."() {
+        given:
+        Item nuggets = new Item("Nuggets", 185.0, ItemType.FOOD)
+        Item ham = new Item("Ham", 198.0, ItemType.FOOD)
+        Item biscuit = new Item("Biscuit", 49.0, ItemType.GADGET)
+
+        List<Item> items = [nuggets, ham, biscuit]
+
+        Cart itemsInTheCart = new Cart(UUID.randomUUID(), items)
+
+        when:
+        boolean eligibleForDiscount = service.isCartEligibleForDiscount(itemsInTheCart)
+
+        then:
+        !eligibleForDiscount
+
+    }
+
+    def "applyDiscountToCartItems should apply the discount to each items from the Cart."() {
+        given:
+            Item nuggets = new Item("Nuggets", 185.0, ItemType.FOOD)
+            Item ham = new Item("Ham", 198.0, ItemType.FOOD)
+            Item biscuit = new Item("Biscuit", 49.0, ItemType.GADGET)
+            Item appleJuice = new Item("Apple Juice", 174.0, ItemType.FOOD)
+            Item milk = new Item("Milk", 98.0, ItemType.FOOD)
+            Item chocolate = new Item("Chocolate", 357.0, ItemType.FOOD)
+
+            List<Item> items = [nuggets, ham, biscuit, appleJuice, milk, chocolate]
+
+            Cart itemsInTheCart = new Cart(UUID.randomUUID(), items)
+
+        when:
+            double discountedCost = service.applyDiscountToCartItems(itemsInTheCart)
+
+        then:
+            discountedCost
+
+    }
+
 }
