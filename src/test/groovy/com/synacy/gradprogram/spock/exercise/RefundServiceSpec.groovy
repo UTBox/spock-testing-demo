ffa9066a55e1c1a5a5ee119fc9ad087bab725f46
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-22 22:04:07 +0800

    Added unit test using TDD for calculateRefund

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -0,0 +1,29 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class RefundServiceSpec extends Specification {
+
+    RefundService service
+    OrderingService orderingService = Mock(OrderingService)
+
+    void setup() {
+        service = new RefundService()
+    }
+
+    def "calculateRefund should give full refund due to damage item cancel reason"() {
+        given:
+        List<Item> items = [new Item("Fried Chicken", 50.00d, ItemType.FOOD),
+                            new Item("Nike", 500.00d, ItemType.CLOTHING),
+                            new Item("Rice Cooker", 300.75d, ItemType.APPLIANCE),
+                            new Item("IPhone", 23000.00d, ItemType.GADGET)]
+        Cart cart = new Cart(UUID.randomUUID(), items)
+        orderingService.calculateTotalCostOfCart(cart) >> 23650.75d
+
+        when:
+        BigDecimal refundAmount = service.calculateRefund(CancelReason.DAMAGED, cart)
+
+        then:
+        refundAmount == BigDecimal.valueOf(23650.75d)
+    }
+}
