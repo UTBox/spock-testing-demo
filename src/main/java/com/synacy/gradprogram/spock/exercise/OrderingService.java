e6277be66362f8f0c9471e01774a505991e3f2d9
junsaku0 <junsaku.yamada@synacy.com>
2023-07-22 23:42:33 +0800

    Add implementation for OrderingService.cancelOrder - for unit test "cancelOrder should throw UnableToCancelException if OrderStatus is not PENDING or FOR_DELIVERY"

--- src/main/java/com/synacy/gradprogram/spock/exercise/OrderingService.java
+++ src/main/java/com/synacy/gradprogram/spock/exercise/OrderingService.java
@@ -66,11 +66,12 @@ public class OrderingService {
     return order;
   }
 
-  public void cancelOrder(CancelOrderRequest request, Order order) {
+  public void cancelOrder(CancelOrderRequest request, Order order) throws UnableToCancelException {
     // TODO: Implement me. Cancels PENDING and FOR_DELIVERY orders and create a refund request saving it to the database.
     if (order.getStatus() == OrderStatus.PENDING || order.getStatus() == OrderStatus.FOR_DELIVERY) {
       order.setStatus(OrderStatus.CANCELLED);
+    } else {
+      throw new UnableToCancelException();
     }
-    //  Else throws an UnableToCancelException
   }
 }
