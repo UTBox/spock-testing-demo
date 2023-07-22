7f2c473c81cd09ce9b940d416450fd85c675d274
junsaku0 <junsaku.yamada@synacy.com>
2023-07-22 16:37:19 +0800

    Create unit test for RefundService.calculateRefund for CancelReason.DAMAGED

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -0,0 +1,26 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class RefundServiceSpec extends Specification {
+
+    RefundService refundService
+    CancelOrderRequest request
+
+    void setup() {
+        refundService = new RefundService()
+
+    }
+
+    def "calculateRefund should return full total cost if cancel reason is DAMAGED"() {
+        given:
+        Order order = new Order(totalCost: 100)
+        request = new CancelOrderRequest(reason: CancelReason.DAMAGED)
+
+        when:
+        BigDecimal refund = refundService.calculateRefund(request)
+
+        then:
+        order.getTotalCost() as BigDecimal == refund
+    }
+}
