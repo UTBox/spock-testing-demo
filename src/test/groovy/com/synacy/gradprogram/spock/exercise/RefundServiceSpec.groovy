c27da8fd239728ae85e61b86be19c1d19f32082c
“Ernestkun” <ernest.gloria@synacy.com>
2023-07-22 21:17:06 +0800

    Added RefundServiceSpec and created a TDD for CalculateRefund Method

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -0,0 +1,11 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class RefundServiceSpec extends Specification {
+    void setup() {
+    }
+
+    def "CalculateRefund"() {
+    }
+}
