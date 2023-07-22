f187cc8a7ae45e55c2d10e3430cccb00c0842e93
junsaku0 <junsaku.yamada@synacy.com>
2023-07-22 21:20:09 +0800

    Refactor RefundService.calculateRefund and add constructor with repository

--- src/main/java/com/synacy/gradprogram/spock/exercise/RefundService.java
+++ src/main/java/com/synacy/gradprogram/spock/exercise/RefundService.java
@@ -4,20 +4,27 @@ import java.math.BigDecimal;
 
 public class RefundService {
 
-  public BigDecimal calculateRefund(CancelOrderRequest request, Order order) {
-    long refundDateLimit = order.getDateOrdered().getTime() + (86400000 * 3);
+  private final RefundRepository refundRepository;
 
-    if (request.getReason() == CancelReason.DAMAGED) {
-      return BigDecimal.valueOf(order.getTotalCost());
-    } else if (request.getDateCancelled().getTime() < refundDateLimit) {
-      return BigDecimal.valueOf(order.getTotalCost());
-    } else {
-      return BigDecimal.valueOf(order.getTotalCost()/2);
-    }
+  public RefundService(RefundRepository refundRepository) {
+    this.refundRepository = refundRepository;
   }
 
-  private void createAndSaveRefundRequest() {
+  public BigDecimal calculateRefund(CancelOrderRequest request, Order order) {
+    long refundDateLimit = order.getDateOrdered().getTime() + (86400000 * 3);
+    BigDecimal refundAmount;
+
+    if (request.getReason() == CancelReason.DAMAGED || request.getDateCancelled().getTime() < refundDateLimit) {
+      refundAmount = BigDecimal.valueOf(order.getTotalCost());
+    } else {
+      refundAmount = BigDecimal.valueOf(order.getTotalCost()/2);
+    }
+    return refundAmount;
+  }
+
+  private void createAndSaveRefundRequest(Order order) {
     // TODO: Implement me. Creates a TO_PROCESS refund request and saves it to the database
+
   }
 
 }
