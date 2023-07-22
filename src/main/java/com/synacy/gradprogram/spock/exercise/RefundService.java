da3982f372e7f1c0cbc92d6bba60a17af443f3a6
“Ernestkun” <ernest.gloria@synacy.com>
2023-07-22 22:28:32 +0800

    Succesfuly tested CalculateRefund method and added daysBetweenMethod to calculate the number of days between two Date objects: startDate and endDate.

--- src/main/java/com/synacy/gradprogram/spock/exercise/RefundService.java
+++ src/main/java/com/synacy/gradprogram/spock/exercise/RefundService.java
@@ -1,13 +1,30 @@
 package com.synacy.gradprogram.spock.exercise;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class RefundService {
 
-  public BigDecimal calculateRefund() {
-    // TODO: Implement me. Full refund if cancel reason is due to damaged item.
-    //  Also full refund if the order was cancelled within 3 days of order date, else refund half of the total cost.
-    return null;
+  private RefundRepository refundRepository;
+  private RefundRequestStatus refundRequestStatus;
+  public RefundService(RefundRepository refundRepository, RefundRequestStatus refundRequestStatus) {
+    this.refundRepository = refundRepository;
+    this.refundRequestStatus = refundRequestStatus;
+  }
+  public BigDecimal calculateRefund(Date orderDate, CancelReason cancelReason, BigDecimal totalCost) {
+
+    long daysSinceOrder = daysBetween(orderDate, new Date());
+
+    if (cancelReason == CancelReason.DAMAGED || daysSinceOrder <=3){
+      return totalCost;
+    }else {
+      return totalCost.divide(new BigDecimal("2"));
+    }
+
+  }
+  private long daysBetween(Date startDate, Date endDate) {
+    long diff = endDate.getTime() - startDate.getTime();
+    return diff / (24 * 60 * 60 * 1000);
   }
 
   private void createAndSaveRefundRequest() {
--- src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -3,9 +3,44 @@ package com.synacy.gradprogram.spock.exercise
 import spock.lang.Specification
 
 class RefundServiceSpec extends Specification {
+
+
+    RefundRepository refundRepository = Mock(RefundRepository)
+    RefundRequest refundRequest = Mock(RefundRequest)
+    RefundRequestStatus refundRequestStatus = GroovyMock(RefundRequestStatus)
+    RefundService refundService
+
+
     void setup() {
+        refundService = new RefundService(refundRepository,refundRequestStatus)
     }
 
-    def "CalculateRefund"() {
+    def "Should return full refund if the item is Damaged"() {
+        given:
+        refundRequest.isDamagedItem() >> true
+        refundService.daysBetween(_,_) >> 2
+        BigDecimal totalCost = new BigDecimal("100")
+
+        when:
+        BigDecimal totalRefund = refundService.calculateRefund(new Date(), CancelReason.DAMAGED, totalCost)
+
+        then:
+        totalRefund == totalCost
+    }
+
+    def "Should return half refund for other cancellation reasons"() {
+        given:
+        refundRequest.isDamagedItem() >> false
+        refundService.daysBetween(_, _) >> 4
+        refundRequest.getCancelReason() >> CancelReason.WRONG_ITEM
+        BigDecimal totalCost = new BigDecimal("50.00")
+        BigDecimal expectedRefund = totalCost.divide(new BigDecimal("2"))
+
+
+        when:
+        BigDecimal totalRefund = refundService.calculateRefund(new Date(), CancelReason.WRONG_ITEM, totalCost).divide(new BigDecimal("2"))
+
+        then:
+        totalRefund == expectedRefund
     }
 }
