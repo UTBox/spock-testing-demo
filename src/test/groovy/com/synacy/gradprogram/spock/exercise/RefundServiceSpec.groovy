b3bee6263c81db5ffff646ed424d7008fac45487
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-23 17:34:50 +0800

    Added TTD for other cancel reason

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -38,4 +38,17 @@ class RefundServiceSpec extends Specification {
         result == BigDecimal.valueOf(100.0)
         refundRequest.refundAmount == BigDecimal.valueOf(100.0)
     }
+
+    def "calculateRefund should give half refund for other cancel reason"() {
+        given:
+        CancelReason cancelReason = CancelReason.WRONG_ITEM
+        Order order = new Order(totalCost: 100.0, dateOrdered: DateUtils.subtractDays(new Date(), 5))
+
+        when:
+        BigDecimal result = service.calculateRefund(cancelReason, order, refundRequest)
+
+        then:
+        result == BigDecimal.valueOf(50.0)
+        refundRequest.refundAmount = BigDecimal.valueOf(50.0)
+    }
 }
