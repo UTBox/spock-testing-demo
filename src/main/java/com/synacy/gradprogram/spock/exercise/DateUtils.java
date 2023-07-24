241e5a0c83940c905db5fc6ffd26636d92b4d1bf
Carlina-Amaba <carlina.amaba@synacy.com>
2023-07-25 01:27:51 +0800

    Added unit tests for RefundServiceSpec

--- src/main/java/com/synacy/gradprogram/spock/exercise/DateUtils.java
+++ src/main/java/com/synacy/gradprogram/spock/exercise/DateUtils.java
@@ -1,6 +1,7 @@
 package com.synacy.gradprogram.spock.exercise;
 
 import java.util.Date;
+import java.util.Calendar;
 
 public class DateUtils {
 
@@ -8,4 +9,9 @@ public class DateUtils {
     return new Date();
   }
 
+  public static Date getDaysBeforeDate(int days) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.add(Calendar.DATE,days);
+    return calendar.getTime();
+  }
 }
--- src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -3,10 +3,65 @@ package com.synacy.gradprogram.spock.exercise
 import spock.lang.Specification
 
 class RefundServiceSpec extends Specification {
+    RefundService refundService
+    RefundRepository refundRepository = Mock(RefundRepository)
+
     void setup() {
+        refundService = new RefundService()
     }
 
-    def "CalculateRefund should return full refund due to damaged item"() {
+    def "CalculateRefund should return full refund if order is a damaged item"() {
+        given:
+        Order order = new Order(status: OrderStatus.CANCELLED, totalCost: 100.0)
+        CancelOrderRequest cancelOrderRequest = new CancelOrderRequest(reason: CancelReason.DAMAGED)
+        BigDecimal fullAmount = 100
 
+        when:
+        BigDecimal refund = refundService.calculateRefund(cancelOrderRequest, order)
+
+        then:
+        fullAmount == refund
     }
+
+
+    def "CalculateRefund should return full refund if order was cancelled within 3 days of order date"() {
+        given:
+        Order order = new Order(status: OrderStatus.CANCELLED, totalCost: 100.0, dateOrdered: DateUtils.getDaysBeforeDate(-2))
+        CancelOrderRequest cancelOrderRequest = new CancelOrderRequest(reason: CancelReason.DAMAGED)
+        BigDecimal fullAmount = 100
+
+        when:
+        BigDecimal refund = refundService.calculateRefund(cancelOrderRequest, order)
+
+        then:
+        fullAmount == refund
+    }
+
+    def "CalculateRefund should return half refund if order is not a damaged item"() {
+        given:
+        Order order = new Order(status: OrderStatus.CANCELLED, totalCost: 100.0)
+        CancelOrderRequest cancelOrderRequest = new CancelOrderRequest(reason: CancelReason.WRONG_ITEM)
+        BigDecimal halfAmount = 50
+
+        when:
+        BigDecimal refund = refundService.calculateRefund(cancelOrderRequest, order)
+
+        then:
+        halfAmount == refund
+    }
+
+    def "CalculateRefund should return half refund if order was cancelled after 3 days of order date"() {
+        given:
+        Order order = new Order(status: OrderStatus.CANCELLED, totalCost: 100.0, dateOrdered: DateUtils.getDaysBeforeDate(-5))
+        CancelOrderRequest cancelOrderRequest = new CancelOrderRequest(reason: CancelReason.DAMAGED)
+        BigDecimal halfAmount = 50
+
+        when:
+        BigDecimal refund = refundService.calculateRefund(cancelOrderRequest, order)
+
+        then:
+        halfAmount == refund
+    }
+
+
 }
