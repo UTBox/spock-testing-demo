8f49743f7812e7f2d80e64dfdaf2d416dec4f49f
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-24 12:45:40 +0800

    Add TDD for cancelOrder method

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceSpec.groovy
@@ -0,0 +1,34 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class OrderingServiceSpec extends Specification {
+
+    OrderingService orderingService
+    OrderRepository orderRepository = Mock(OrderRepository)
+    RefundService refundService = Mock(RefundService)
+
+    void setup() {
+        orderingService = new OrderingService(orderRepository)
+    }
+
+    def "cancelOrder should cancel PENDING and FOR_DELIVERY order and create refund amount and refund request"() {
+        given:
+        Order order = new Order(totalCost: 100.0d, recipientName: "Romeo", status: status)
+        CancelOrderRequest cancelOrderRequest = new CancelOrderRequest()
+        BigDecimal refundAmount = refundService.calculateRefund(cancelOrderRequest.getReason(), order)
+
+        when:
+        orderingService.cancelOrder(new CancelOrderRequest(), order)
+
+        then:
+        1 * refundService.createAndSaveRefundRequest(order.id, order.recipientName, refundAmount)
+        1 * orderRepository.saveOrder(order)
+
+        and:
+        order.status == OrderStatus.CANCELLED
+
+        where:
+        status << [OrderStatus.PENDING, OrderStatus.FOR_DELIVERY]
+    }
+}
