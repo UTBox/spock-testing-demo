68630e14ad176af2f4f82a30632839f86d8df289
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-24 10:31:02 +0800

    Mock the refund request

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -5,20 +5,20 @@ import spock.lang.Specification
 class RefundServiceSpec extends Specification {
 
     RefundService service
-    RefundRequest refundRequest
+    RefundRequest refundRequest = Mock(RefundRequest)
     RefundRepository refundRepository = Mock(RefundRepository)
     void setup() {
-        service = new RefundService(refundRepository)
-        refundRequest = new RefundRequest()
+        service = new RefundService(refundRepository, refundRequest)
     }
 
     def "calculateRefund should give full refund due to damage item cancel reason"() {
         given:
         CancelReason cancelReason = CancelReason.DAMAGED
         Order order = new Order(totalCost: 100.0, dateOrdered: new Date())
+        refundRequest = new RefundRequest(recipientName: "Romeo", orderId: UUID.randomUUID(), refundAmount: BigDecimal.valueOf(100.0d), status: RefundRequestStatus.TO_PROCESS)
 
         when:
-        BigDecimal result = service.calculateRefund(cancelReason, order, refundRequest)
+        BigDecimal result = service.calculateRefund(cancelReason, order)
 
         then:
         result == BigDecimal.valueOf(100.0)
@@ -29,10 +29,11 @@ class RefundServiceSpec extends Specification {
         given:
         CancelReason cancelReason = CancelReason.DAMAGED
         Order order = new Order(totalCost: 100.0, dateOrdered: DateUtils.subtractDays(new Date(), 3))
+        refundRequest = new RefundRequest(recipientName: "Romeo", orderId: UUID.randomUUID(), refundAmount: BigDecimal.valueOf(100.0d), status: RefundRequestStatus.TO_PROCESS)
         boolean isMoreThanThreeDays = DateUtils.isMoreThanThreeDaysAgo(order.dateOrdered)
 
         when:
-        BigDecimal result = service.calculateRefund(cancelReason, order, refundRequest)
+        BigDecimal result = service.calculateRefund(cancelReason, order)
 
         then:
         isMoreThanThreeDays
@@ -44,9 +45,10 @@ class RefundServiceSpec extends Specification {
         given:
         CancelReason cancelReason = CancelReason.WRONG_ITEM
         Order order = new Order(totalCost: 100.0, dateOrdered: DateUtils.subtractDays(new Date(), 5))
+        refundRequest = new RefundRequest(recipientName: "Romeo", orderId: UUID.randomUUID(), refundAmount: BigDecimal.valueOf(100.0d), status: RefundRequestStatus.TO_PROCESS)
 
         when:
-        BigDecimal result = service.calculateRefund(cancelReason, order, refundRequest)
+        BigDecimal result = service.calculateRefund(cancelReason, order)
 
         then:
         result == BigDecimal.valueOf(50.0)
