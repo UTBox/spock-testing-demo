7f890c2c80144716c0e1e5c3da23b6936f31abef
BlankDesu13 <romeo.fermano@synacy.com>
2023-07-24 10:34:32 +0800

    Remve unit test for private methods

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -54,23 +54,4 @@ class RefundServiceSpec extends Specification {
         result == BigDecimal.valueOf(50.0)
         refundRequest.refundAmount == BigDecimal.valueOf(50.0)
     }
-
-    def "createAndSaveRefundRequest should create and save refund request"() {
-        given:
-        UUID orderId = UUID.randomUUID()
-        String recipientName = "Romeo"
-        BigDecimal refundAmount = BigDecimal.valueOf(100.0)
-        RefundRequestStatus refundRequestStatus = RefundRequestStatus.TO_PROCESS
-
-        when:
-        service.createAndSaveRefundRequest(orderId, recipientName, refundAmount)
-
-        then:
-        1 * refundRepository.saveRefundRequest(_) >> { RefundRequest refundRequest ->
-            assert orderId == refundRequest.getOrderId()
-            assert recipientName == refundRequest.getRecipientName()
-            assert refundAmount == refundRequest.getRefundAmount()
-            assert refundRequestStatus == refundRequest.getStatus()
-        }
-    }
 }
