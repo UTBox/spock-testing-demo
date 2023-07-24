a9ccac58b512e9cc686f1e917c46431d03b61734
junsaku0 <junsaku.yamada@synacy.com>
2023-07-24 22:49:02 +0800

    Refactor caculation for Cancel Date Limit

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -28,10 +28,12 @@ class RefundServiceSpec extends Specification {
     def "calculateRefund should return full total cost if dateCancelled is within 3 days of dateOrdered"() {
         given:
         Date dateOrdered = dateUtils.getCurrentDate()
-        Date dateLimit = new Date(dateOrdered.getTime() + (86400000 * 3) - 1)
+        Long millisecondsInThreeDays = 86400000 * 3
+        Long millisecondsBeforeCancelLimit = dateOrdered.getTime() + millisecondsInThreeDays - 1
+        Date dateBeforeCancelLimit = new Date(millisecondsBeforeCancelLimit)
 
         Order order = new Order(totalCost: 100, dateOrdered: dateOrdered)
-        CancelOrderRequest request = new CancelOrderRequest(dateCancelled: dateLimit)
+        CancelOrderRequest request = new CancelOrderRequest(dateCancelled: dateBeforeCancelLimit)
 
         when:
         BigDecimal refund = refundService.calculateRefund(request, order)
@@ -43,10 +45,12 @@ class RefundServiceSpec extends Specification {
     def "calculateRefund should return half of the total cost for dateCancelled more than 3 days after dateOrdered"() {
         given:
         Date dateOrdered = dateUtils.getCurrentDate()
-        Date dateLimit = new Date(dateOrdered.getTime() + (86400000 * 3) + 1)
+        Long millisecondsInThreeDays = 86400000 * 3
+        Long millisecondsAfterCancelLimit = dateOrdered.getTime() + millisecondsInThreeDays + 1
+        Date dateAfterCancelLimit = new Date(millisecondsAfterCancelLimit)
 
         Order order = new Order(totalCost: 100, dateOrdered: dateOrdered)
-        CancelOrderRequest request = new CancelOrderRequest(dateCancelled: dateLimit)
+        CancelOrderRequest request = new CancelOrderRequest(dateCancelled: dateAfterCancelLimit)
 
         when:
         BigDecimal refund = refundService.calculateRefund(request, order)
