28bf5780972f924a6e0e3019a46d1ed0d135bf24
alwyndy <alwyn.dy@synacy.com>
2024-07-26 15:26:00 +0800

    Add UnableToCancelException unit test for cancelOrder

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceSpec.groovy
@@ -0,0 +1,36 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class OrderingServiceSpec extends Specification {
+
+    OrderingService orderingService
+
+    OrderRepository orderRepository = Mock()
+
+    def setup(){
+        orderingService = new OrderingService(orderRepository)
+    }
+
+    def "cancelOrder should throw an UnableToCancelException for orders with #status status"(){
+        given:
+        UUID uuid = UUID.randomUUID()
+        Order order = Mock()
+        order.getStatus() >> status
+        order.getId() >> uuid
+
+        and:
+        CancelOrderRequest request = Mock()
+        request.getOrderId() >> uuid
+        orderRepository.fetchOrderById(uuid) >> order
+
+        when:
+        orderingService.cancelOrder(request)
+
+        then:
+        thrown(UnableToCancelException)
+
+        where:
+        status << [OrderStatus.CANCELLED, OrderStatus.DELIVERED]
+    }
+}
