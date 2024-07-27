934f64c0e3ba6c12f38dbaa047bd5bd2e6932779
JuliusF-117 <julius.fabrique@synacy.com>
2024-07-27 19:25:42 +0800

    - Create unit test files 	- RefundServiceSpec 	- OrderingServiceSpec - Create new exception UnableToCancelException

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceSpec.groovy
@@ -0,0 +1,12 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class OrderingServiceSpec extends Specification {
+
+
+
+    def "cancelOrder"() {
+
+    }
+}
---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -0,0 +1,12 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class RefundServiceSpec extends Specification {
+
+
+
+    def "calculateRefund"() {
+
+    }
+}
