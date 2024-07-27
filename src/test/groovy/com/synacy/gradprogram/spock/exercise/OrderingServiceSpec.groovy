17283d7390e0389e47675d6ad443b21c8a395ca4
seancapulong <sean.capulong@synacy.com>
2024-07-27 16:49:04 +0800

    Test: Add test implementation for cancelOrder method when request order is eligible for cancelling

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/OrderingServiceSpec.groovy
@@ -5,9 +5,10 @@ import spock.lang.Specification
 class OrderingServiceSpec extends Specification {
     OrderingService orderingService
     OrderRepository orderRepository = Mock(OrderRepository)
+    RefundRepository refundRepository = Mock(RefundRepository)
 
     def setup() {
-        orderingService = new OrderingService(orderRepository)
+        orderingService = new OrderingService(orderRepository, refundRepository)
     }
 
     def "cancelOrder should throw an UnableToCancelException when request order status is invalid"() {
@@ -31,5 +32,27 @@ class OrderingServiceSpec extends Specification {
 
         where:
         orderStatus << [OrderStatus.DELIVERED, OrderStatus.CANCELLED]
+    }
+
+    def "cancelOrder should cancel order and create refund request when request order status is valid"() {
+        given:
+        Order order = Mock(Order)
+        UUID id = UUID.randomUUID()
+        order.id >> id
+        order.status >> orderStatus
+
+        orderRepository.fetchOrderById(id) >> Optional.of(order)
+
+        CancelOrderRequest cancelOrderRequest = Mock(CancelOrderRequest)
+        cancelOrderRequest.orderId >> id
+
+        when:
+        orderingService.cancelOrder(cancelOrderRequest)
+
+        then:
+        1 * refundRepository.saveRefundRequest(_ as RefundRequest)
+
+        where:
+        orderStatus << [OrderStatus.PENDING, OrderStatus.FOR_DELIVERY]
     }
 }
