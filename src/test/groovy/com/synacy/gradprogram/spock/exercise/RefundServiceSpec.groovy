30a3335e2152c6eb151cf37a4b44e100f0b747be
seancapulong <sean.capulong@synacy.com>
2024-07-28 12:35:38 +0800

    Add unit test to calculateRefund method in RefundService class

---
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/RefundServiceSpec.groovy
@@ -0,0 +1,62 @@
+package com.synacy.gradprogram.spock.exercise
+
+import spock.lang.Specification
+
+class RefundServiceSpec extends Specification {
+    RefundService refundService
+
+    OrderRepository orderRepository = Mock(OrderRepository)
+
+    void setup() {
+        refundService = new RefundService(orderRepository)
+    }
+
+    def "calculateRefund should return full refund amount when cancel reason is due to damaged item "() {
+        given:
+        RefundRequest refundRequest = Mock()
+        CancelOrderRequest cancelOrderRequest = Mock()
+
+        BigDecimal expectedRefundAmount = BigDecimal.valueOf(100)
+        refundRequest.refundAmount >> expectedRefundAmount
+        cancelOrderRequest.reason >> CancelReason.DAMAGED
+
+        when:
+        BigDecimal actualRefundAmount = refundService.calculateRefund(refundRequest, cancelOrderRequest)
+
+        then:
+        expectedRefundAmount == actualRefundAmount
+    }
+
+    def "calculateRefund should return full refund amount when cancel reason is wrong item and order is cancelled within 3 days of order date"() {
+        given:
+        UUID orderId = UUID.randomUUID()
+        Order order = Mock(Order)
+        order.id >> orderId
+
+        and:
+        Calendar calendar = Calendar.getInstance()
+        calendar.add(Calendar.DATE, - 3)
+        Date modifiedOrderDate = calendar.getTime()
+        order.getDateOrdered() >> modifiedOrderDate
+
+        orderRepository.fetchOrderById(orderId) >> Optional.of(order)
+
+        and:
+        RefundRequest refundRequest = Mock(RefundRequest)
+        refundRequest.orderId >> orderId
+        BigDecimal expectedRefundAmount = BigDecimal.valueOf(100)
+        refundRequest.refundAmount >> expectedRefundAmount
+
+        and:
+        CancelOrderRequest cancelOrderRequest = Mock(CancelOrderRequest)
+        cancelOrderRequest.reason >> CancelReason.WRONG_ITEM
+        Date cancelDate = new Date()
+        cancelOrderRequest.dateCancelled >> cancelDate
+
+        when:
+        BigDecimal actualRefund = refundService.calculateRefund(refundRequest, cancelOrderRequest)
+
+        then:
+        expectedRefundAmount == actualRefund
+    }
+}
