99dce85338ce12d2391250d40708da626ac289d0
seancapulong <sean.capulong@synacy.com>
2024-08-01 17:15:30 +0800

    Refactor DeliveryServiceSpec
    Refactor test descriptions and moved mocks outside setup
    
    Add separate test for determineCourier in createDelivery method

--- src/test/groovy/com/synacy/gradprogram/spock/exercise/DeliveryServiceSpec.groovy
+++ src/test/groovy/com/synacy/gradprogram/spock/exercise/DeliveryServiceSpec.groovy
@@ -3,24 +3,45 @@ package com.synacy.gradprogram.spock.exercise
 import spock.lang.Specification
 
 class DeliveryServiceSpec extends Specification {
-    DeliveryRequestRepository deliveryRequestRepository
-    DateUtils dateUtils
-
     DeliveryService deliveryService
 
+    DeliveryRequestRepository deliveryRequestRepository = Mock();
+    DateUtils dateUtils = Mock();
+
     void setup() {
-        deliveryRequestRepository = Mock()
-        dateUtils = Mock()
         deliveryService = new DeliveryService(dateUtils, deliveryRequestRepository)
     }
 
-    def "createDeliveryRequest should save delivery request with the valid order details"() {
+    def "createDelivery should use #courier for total cost #desc "() {
         given:
-        def currentDate = new Date()
+        Date currentDate = new Date()
         dateUtils.currentDate >> currentDate
 
-        def order = new Order()
-        order.totalCost = expectedTotalCost
+        Order order = new Order()
+        order.totalCost = totalCost
+
+        when:
+        deliveryService.createDelivery(order)
+
+        then:
+        1 * deliveryRequestRepository.saveDeliveryRequest(_ as DeliveryRequest) >> { DeliveryRequest passedRequest ->
+            assert courier == passedRequest.courier
+        }
+
+        where:
+        courier      | totalCost | desc
+        Courier.JRS  | 15        | "less than 20"
+        Courier.GRAB | 25        | "greater than or equal to 20 but less than or equal to 30"
+        Courier.LBC  | 35        | "greater than 30"
+    }
+
+    def "createDelivery should save delivery request with the correct details"() {
+        given:
+        Date currentDate = new Date()
+        dateUtils.currentDate >> currentDate
+
+        Order order = new Order()
+        order.totalCost = 15
 
         when:
         deliveryService.createDelivery(order)
@@ -29,13 +50,7 @@ class DeliveryServiceSpec extends Specification {
         1 * deliveryRequestRepository.saveDeliveryRequest(_ as DeliveryRequest) >> { DeliveryRequest passedRequest ->
             assert order.id == passedRequest.orderId
             assert currentDate == passedRequest.deliveryDate
-            assert expectedCourier == passedRequest.courier
+            assert Courier.JRS == passedRequest.courier
         }
-
-        where:
-        expectedCourier | expectedTotalCost | desc
-        Courier.JRS     | 15                | "less than 20"
-        Courier.GRAB    | 25                | "greater than or equal to 20 but less than or equal to 30"
-        Courier.LBC     | 35                | "greater than 30"
     }
 }
